8cb44ef0c49d1f02f4592b2f3782f3ca70f372e3
Emelie Sannerfelt <emelie.sannerfelt@hotmail.com>
2021-05-04 14:10:47 +0200

    Added 2 tests

--- cypress/integration/assignment03.js
+++ cypress/integration/assignment03.js
@@ -1,13 +1,32 @@
 /// <reference types="cypress" />
 
 
-describe('Test suite with Page-objects - Assignment02', () =>{
+describe('Tester Emelie', () =>{
 
-    beforeEach ('Visit website and log in', () => {
-    })
+    it("(Login)", () => {
+        cy.visit("http://localhost:3000");
+        cy.get('h2').should("contain", "Login")
+        cy.get(':nth-child(1) > input').type('tester01')
+        cy.get(':nth-child(2) > input').type('GteteqbQQgSr88SwNExUQv2ydb7xuf8c')
+        cy.get('.btn').click()
+        cy.get(".username").should("contain", "Welcome tester01!")
+        cy.get("h2").should("contain", "Tester Hotel Overview")
+        cy.get(".user > .btn").click()
+        cy.get("h2").should("contain", "Login")
+    });
 
-    afterEach ('Logout', () => {
-    })
+    it("(Logout)", () => {
+        cy.visit("http://localhost:3000");
+        cy.get('h2').should("contain", "Login")
+        cy.get(':nth-child(1) > input').type('tester01')
+        cy.get(':nth-child(2) > input').type('GteteqbQQgSr88SwNExUQv2ydb7xuf8c')
+        cy.get('.btn').click()
+        cy.get(".username").should("contain", "Welcome tester01!")
+        cy.get("h2").should("contain", "Tester Hotel Overview")
+        cy.get(".user > .btn").click()
+        cy.get("h2").should("contain", "Login")
+    });
+    
 
     it('', () => {
 
