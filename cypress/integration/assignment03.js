2ab7c335a0b49183c70a0d1b176917710f926264
Emelie Sannerfelt <emelie.sannerfelt@hotmail.com>
2021-05-04 14:20:03 +0200

    Added 2 more tests

--- cypress/integration/assignment03.js
+++ cypress/integration/assignment03.js
@@ -26,10 +26,39 @@ describe('Tester Emelie', () =>{
         cy.get(".user > .btn").click()
         cy.get("h2").should("contain", "Login")
     });
-    
 
-    it('', () => {
+    it("(Create a bill)", () => {
+        cy.visit("http://localhost:3000");
+        cy.get('h2').should("contain", "Login")
+        cy.get(':nth-child(1) > input').type('tester01')
+        cy.get(':nth-child(2) > input').type('GteteqbQQgSr88SwNExUQv2ydb7xuf8c')
+        cy.get('.btn').click()
+        cy.get(".username").should("contain", "Welcome tester01!")
+        cy.get("h2").should("contain", "Tester Hotel Overview")
+        cy.get(':nth-child(3) > .btn').click();
+        cy.get('h2').should("contain", "Bills")
+        cy.get('h2 > .btn').click();
+        cy.get("h2").should("contain", "New Bill")
+        cy.get('input').type("1000")
+        cy.get('.checkbox').click();
+        cy.get('.blue').click();
+        cy.get("h2").should("contain", "Bills")
+        cy.get('.user > .btn').click()
+    });
 
-    })
+    it("(Delete a bill)", () => {
+        cy.visit("http://localhost:3000");
+        cy.get('h2').should("contain", "Login")
+        cy.get(':nth-child(1) > input').type('tester01')
+        cy.get(':nth-child(2) > input').type('GteteqbQQgSr88SwNExUQv2ydb7xuf8c')
+        cy.get('.btn').click()
+        cy.get(".username").should("contain", "Welcome tester01!")
+        cy.get("h2").should("contain", "Tester Hotel Overview")
+        cy.get(':nth-child(3) > .btn').click();
+        cy.get(':nth-child(3) > .action > img').click();
+        cy.get('.menu > :nth-child(2)').click();
+        cy.get('.user > .btn').click();
+
+    });
 
 })
